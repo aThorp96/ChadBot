f5f0d7c649f00724779c6a160f022e29e0606e37
Alex Wohlbruck <alexwohlbruck@gmail.com>
2018-10-06 15:40:41 -0400

    Add prompts method for output file

--- convertToBytes.py
+++ convertToBytes.py
@@ -1,6 +1,6 @@
 
 def byteArrayOut():
-    with open("C:\\Users\\Jacob Justice\\Documents\\hacknc 2018\\happylee-supermariobros,warped.fm2") as f:
+    with open("happylee-supermariobros,warped.fm2") as f:
         f_array = f.readlines()
 
         results = []
@@ -22,22 +22,56 @@ def strToByte(stringRow):
     if stringRow[5] != '.': count += 4
     if stringRow[6] != '.': count += 2
     if stringRow[7] != '.': count += 1
-    return
+
+    return count
 
 
 def byteToStr(byte):
-  buttons = 'RLDUTSBA'
-  binString = '{0:08b}'.format(byte)
-  buttonString = ''
+    buttons = 'RLDUTSBA'
+    binString = '{0:08b}'.format(byte)
+    buttonString = ''
 
-  for i, digit in enumerate(binString):
-    if digit == '1':
-      buttonString += buttons[i]
-    else:
-      buttonString += '.'
+    for i, digit in enumerate(binString):
+        if digit == '1':
+            buttonString += buttons[i]
+        else:
+            buttonString += '.'
 
-  return buttonString
+    return buttonString
 
-print(byteArrayOut())
+# print(byteArrayOut())
   
-print(byteToStr(0x82))
+# print(byteToStr(0x82))
+
+def generateFM2(byteArray):
+    file = open('output.fm2', 'w')
+
+    metadata = {
+        "version": input('Version (required): '),
+        "emuVersion": input('Emulator version (required): '),
+        "rerecordCount": input('Re-record count: '),
+        "palFlag": input('Pal flag: '),
+        "romFilename": input('ROM file name (required): '),
+        "romChecksum": input('ROM Checksum (required): '),
+        "guid": input('GUID (required): '),
+        "fourscore": input('Fourscore: '),
+        "microphone": input('Microphone: '),
+        "port0": input('Port 0: '),
+        "port1": input('Port 1: '),
+        "port2": input('Port 2 (required): '),
+        "FDS": input('FDS: '),
+        "NewPPU": input('New PPU: '),
+        "comment author": input('Comment: ')
+    }
+
+    for field, value in metadata.items():
+        if bool(value):
+            file.write(field + ' ' + value + '\n')
+    
+    for i, byte in enumerate(byteArray):
+        prefix = '|1|' if i == 0 else '|0|'
+
+        file.write(prefix + byteToStr(byte) + '|........||' + '\n')
+
+
+generateFM2(byteArrayOut())
