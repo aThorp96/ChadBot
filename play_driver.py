2a7e46d62cb5ff36760a4e54710063506321b85b
JacobJustice <36245913+JacobJustice@users.noreply.github.com>
2018-10-06 21:59:35 -0400

    small change to make it work

--- play_driver.py
+++ play_driver.py
@@ -12,36 +12,37 @@ B = LED(19)
 A = LED(21)
 
 
-def output(byte):
-    if 0x80 & byte != 0:
-        R.on()
-    else:
-        R.off()
-    if 0x40 & byte != 0:
-        L.on()
-    else:
-        L.off()
-    if 0x20 & byte != 0:
-        D.on()
-    else:
-        D.off()
-    if 0x10 & byte != 0:
-        U.on()
-    else:
-        U.off()
-    if 0x08 & byte != 0:
-        T.on()
-    else:
-        T.off()
-    if 0x04 & byte != 0:
-        S.on()
-    else:
-        S.off()
-    if 0x02 & byte != 0:
-        B.on()
-    else:
-        B.off()
-    if 0x01 & byte != 0:
-        A.on()
-    else:
-        A.off()
+def output(byte_array):
+    for byte in byte_array:
+        if 0x80 & byte != 0:
+            R.on()
+        else:
+            R.off()
+        if 0x40 & byte != 0:
+            L.on()
+        else:
+            L.off()
+        if 0x20 & byte != 0:
+            D.on()
+        else:
+            D.off()
+        if 0x10 & byte != 0:
+            U.on()
+        else:
+            U.off()
+        if 0x08 & byte != 0:
+            T.on()
+        else:
+            T.off()
+        if 0x04 & byte != 0:
+            S.on()
+        else:
+            S.off()
+        if 0x02 & byte != 0:
+            B.on()
+        else:
+            B.off()
+        if 0x01 & byte != 0:
+            A.on()
+        else:
+            A.off()
